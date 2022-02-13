a3e10008698adfbe8492867ab4b14406250ce0bc
IanPC <iancaminhas25@hotmail.com>
2022-02-13 08:41:57 -0300

    Variaveis com let, const e var

---
+++ 1-VariaveisEConstantes.js
@@ -0,0 +1,28 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+/*
+com var posso redeclarar as variaveis 2 vezes, exemplo:
+    var a = 1
+    var a = 2
+
+Entretanto, não posso redeclarar a variável 2 vezes, exemplo:
+let a =1
+let a = 2
+Isso dá error
+
+const c = 4
+c = 5 
+Isso dá error, pois constante não pode ser alterada.
+
+*/
