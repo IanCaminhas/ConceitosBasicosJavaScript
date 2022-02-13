dfb7e17da94303124d303dde63011345224e5069
IanPC <iancaminhas25@hotmail.com>
2022-02-13 08:53:28 -0300

    Tipagem fraca

---
+++ 2-TipagemFraca.js
@@ -0,0 +1,19 @@
+let qualquer = 'legal'
+
+console.log(qualquer) //legal
+console.log(typeof qualquer)//string
+
+qualquer = 3.1516
+console.log(qualquer)//3.1516
+console.log(typeof qualquer)//number
+
+/*
+    JavaScript tem tipagem fraca, 
+    ou seja, consigo atribuir diferentes
+    tipos de dados a uma variável.
+    Observe que a variável qualquer recebeu uma string
+    e depois um ponto flutuante.
+*/
+
+
+
